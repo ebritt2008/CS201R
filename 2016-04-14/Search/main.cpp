6d88e248edecbf1269d713d3f8793ded77cbffb5
ebritt2008 <ebritt2008@gmail.com>
2016-04-14 18:35:44 -0500

    File update Search, Find Empty

--- 2016-04-14/Search/main.cpp
+++ 2016-04-14/Search/main.cpp
@@ -4,20 +4,35 @@
 #include <string>
 using namespace std;
 
+
+int Find(string myArray[], int arraySize, const string& searchFor, int index)
+{
+	if (index == arraySize)
+	{
+		return -1;
+	}
+	if (myArray[index] == searchFor)
+	{
+		return index;
+	}
+	return Find(myArray, arraySize, searchFor, index + 1);
+}
+
 int main()
 {
-    string dogBreeds = {
-        "corgi", "labrador", "bulldog", "beagle", "poodle", "boxer"
-        };
+	string dogBreeds[] =
+	{
+		"corgi", "labrador", "bulldog", "beagle", "poodle", "boxer"
+	};
 
     string searchFor;
     cout << "Search for what term? ";
     cin >> searchFor;
-
+	
     int foundIndex = -1;
     for ( int i = 0; i < 6; i++ )
     {
-        if ( dogBreeds[i] == searchFor )
+        if (dogBreeds[i] == searchFor)
         {
             foundIndex = i;
         }
